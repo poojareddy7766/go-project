12b1718c5797f5b6c9df2481ca3acac2f56db899
Pooja M <pooja.m@alteryx.com>
2024-12-27 14:42:07 +0530

    all about post request

--- main.go
+++ main.go
@@ -23,12 +23,26 @@ func main(){
 	// fmt.Println(thirdName)
     fmt.Println("Hello World")
 	app :=fiber.New()
-
-	log.Fatal(app.Listen(":4000"))
 	//fatal is equivalent to print() followed by an equivalent os.Exit(1)
    	///----- All about get request
 	   app.Get("/",func(c *fiber.Ctx) error{
 		return c.Status(200).JSON(fiber.Map{"msg": "Start Understanding get request"})
 	})
+
+	////--- All about post request - create a todo
+    app.Post("/api/todos",func(c *fiber.Ctx) error{
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body"})
+		}
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.Status(201).JSON(todo)
+	})
+
+	log.Fatal(app.Listen(":4000"))
 
 }
