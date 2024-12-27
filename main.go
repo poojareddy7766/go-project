46d91bfe8afad335773e7422a4b180e7a017ec0e
Pooja M <pooja.m@alteryx.com>
2024-12-27 14:42:31 +0530

    understanding pointers in Go

--- main.go
+++ main.go
@@ -40,6 +40,10 @@ func main(){
 		}
 		todo.ID = len(todos) + 1
 		todos = append(todos, *todo)
+		// var x int=5
+		// var p *int=&X
+		// fmt.println(p)
+		// fmt.println(*p)
 		return c.Status(201).JSON(todo)
 	})
 
