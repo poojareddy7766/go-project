78c1dd05a4681afaaf6a8d0464e32e091d379385
Pooja M <pooja.m@alteryx.com>
2024-12-28 13:13:43 +0530

    establish port &  DB connection

--- main.go
+++ main.go
@@ -1,91 +1,146 @@
+// package main
+// import 
+// (
+//     "fmt"
+// 	"log"
+// 	"os"
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/joho/godotenv"
+// )
+
+// type Todo struct {
+// 	ID        int    `json:"id"`
+// 	Completed bool   `json:"completed"`
+// 	Body      string `json:"body"`
+// }
+
+// var todos []Todo 
+// func main(){
+	
+// 	// var myName string= "Pooja reddy"
+// 	// var secondName string="Pooja reddy 2"
+// 	// thirdName := "Pooja reddy 3"
+// 	// fmt.Println(myName)
+// 	// fmt.Println(secondName)
+// 	// fmt.Println(thirdName)
+//     fmt.Println("Hello World")
+// 	app :=fiber.New()
+
+// 	// trying to make port dynamic via .env file
+// 	err := godotenv.Load(".env")
+// 	if err != nil{
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	PORT := os.Getenv("PORT")
+// 	//fatal is equivalent to print() followed by an equivalent os.Exit(1)
+//    	///----- All about get request
+// 	//    app.Get("/",func(c *fiber.Ctx) error{
+// 	// 	return c.Status(200).JSON(fiber.Map{"msg": "Start Understanding get request"})
+// 	// })
+
+// 	// get request
+// 	app.Get("/api/todos",func(c *fiber.Ctx) error{
+// 		return c.Status(200).JSON(todos)
+// 	})
+// 	////--- All about post request - create a todo
+//     app.Post("/api/todos",func(c *fiber.Ctx) error{
+// 		todo := &Todo{}
+// 		if err := c.BodyParser(todo); err != nil {
+// 			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body must not be a string"})
+// 		}
+// 		if todo.Body == "" {
+// 			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+// 		}
+// 		todo.ID = len(todos) + 1
+// 		todos = append(todos, *todo)
+// 		// var x int=5
+// 		// var p *int=&X
+// 		// fmt.println(p)
+// 		// fmt.println(*p)
+// 		return c.Status(201).JSON(todo)
+// 	})
+
+// 	////--- All about updating Todo 
+// 	app.Patch("/api/todos/:id",func(c *fiber.Ctx) error{
+//         id :=c.Params("id")
+		
+// 		for i, todo:=range todos{
+// 			if fmt.Sprint(todo.ID) == id{
+// 				todos[i].Completed = true
+// 				return c.Status(200).JSON(todos[i])
+// 			}
+// 		}
+// 		return c.Status(404).JSON(fiber.Map{"error": "Todo not Found"})
+// 	})
+	
+// 	 // -- Delete a Todo 
+// 	 app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+// 		id := c.Params("id")
+// 		for i, todo := range todos {
+// 			if fmt.Sprint(todo.ID) == id {
+// 				todos = append(todos[:i], todos[i+1:]...) // Fixed closing parenthesis here
+// 				return c.Status(200).JSON(fiber.Map{"Success": "true"})
+// 			}
+// 		}
+// 		return c.Status(404).JSON(fiber.Map{"error": "Todo not Found"})
+// 	})
+   
+// 	log.Fatal(app.Listen(":"+PORT))
+
+// }
+
 package main
 import 
 (
+	"context"
     "fmt"
 	"log"
 	"os"
-	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
+//mongoDb stores data in the form of Binary JSON
 type Todo struct {
-	ID        int    `json:"id"`
+	ID        int    `json:"id" bson: "_id"`
 	Completed bool   `json:"completed"`
 	Body      string `json:"body"`
 }
-
-var todos []Todo 
+var collection *mongo.Collection
 func main(){
-	
-	// var myName string= "Pooja reddy"
-	// var secondName string="Pooja reddy 2"
-	// thirdName := "Pooja reddy 3"
-	// fmt.Println(myName)
-	// fmt.Println(secondName)
-	// fmt.Println(thirdName)
-    fmt.Println("Hello World")
-	app :=fiber.New()
-
-	// trying to make port dynamic via .env file
-	err := godotenv.Load(".env")
+	fmt.Println("Hello World");
+		err := godotenv.Load(".env")
 	if err != nil{
 		log.Fatal("Error loading .env file")
 	}
+	//load and take the uri
+	MONGODB_URI :=os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client,err := mongo.Connect(context.Background(),clientOptions)
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = client.Ping(context.Background(),nil)
 
-	PORT := os.Getenv("PORT")
-	//fatal is equivalent to print() followed by an equivalent os.Exit(1)
-   	///----- All about get request
-	//    app.Get("/",func(c *fiber.Ctx) error{
-	// 	return c.Status(200).JSON(fiber.Map{"msg": "Start Understanding get request"})
-	// })
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("CONNECTED TO MONGODB ATLAS")
+ 
 
-	// get request
-	app.Get("/api/todos",func(c *fiber.Ctx) error{
-		return c.Status(200).JSON(todos)
-	})
-	////--- All about post request - create a todo
-    app.Post("/api/todos",func(c *fiber.Ctx) error{
-		todo := &Todo{}
-		if err := c.BodyParser(todo); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body must not be a string"})
-		}
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
-		}
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo)
-		// var x int=5
-		// var p *int=&X
-		// fmt.println(p)
-		// fmt.println(*p)
-		return c.Status(201).JSON(todo)
-	})
-
-	////--- All about updating Todo 
-	app.Patch("/api/todos/:id",func(c *fiber.Ctx) error{
-        id :=c.Params("id")
-		
-		for i, todo:=range todos{
-			if fmt.Sprint(todo.ID) == id{
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
-		}
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not Found"})
-	})
+	collection=client.Database("golang_db").collection("todos")
+	app :=fiber.New()	
+	app.Get("api/todos",getTodo)
+	app.Post("api/todos",createTodo)
+	app.Patch("api/todos/:i",updateTodo)
+	app.Delete("api/todos/:i",deleteTodo)
 	
-	 // -- Delete a Todo 
-	 app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos = append(todos[:i], todos[i+1:]...) // Fixed closing parenthesis here
-				return c.Status(200).JSON(fiber.Map{"Success": "true"})
-			}
-		}
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not Found"})
-	})
-   
-	log.Fatal(app.Listen(":"+PORT))
+	port := os.Getenv("PORT")
+    if port == ""{
+		port ="5000"
+	}
 
+	log.Fatal(app.Listen("0.0.0.0:"+port))
 }
+
