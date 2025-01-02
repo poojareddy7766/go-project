52c0f37b5760431ca2dbb8e329e60cdec463eb59
Pooja M <pooja.m@alteryx.com>
2025-01-02 14:14:49 +0530

    add files related to ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-import {Button} from "@chakra-ui/react";
-
-function App(){
-  return(
-  <Button>Hello</Button>
-  );
+import {Container, Stack} from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList";
+function App() {
+  return (
+    <Stack h="100vh">
+      <Navbar/>
+       <Container>
+         <TodoForm/>
+         <TodoList/>
+       </Container>
+      </Stack>   
+);
 }
-
 export default App;
